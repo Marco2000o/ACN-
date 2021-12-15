01fc949fdd8a65acc18b39d0513cc4a91b17fb8d
Marco Antonio Martínez Olivares <72892046+Marco2000o@users.noreply.github.com>
2021-12-15 15:16:42 -0600

    PaginaCompleta_ACN

---
+++ js/info-extra.js
@@ -0,0 +1,209 @@
+let hideText_btn = document.getElementById('hideText_btn');
+
+let hideText = document.getElementById('hideText')
+
+hideText_btn.addEventListener('click',toggleText);
+
+function toggleText() {
+    hideText.classList.toggle('show')
+}
+
+
+
+let hideText_btn2 = document.getElementById('hideText_btn2');
+
+let hideText2 = document.getElementById('hideText2')
+
+hideText_btn2.addEventListener('click',toggleText2);
+
+function toggleText2() {
+    hideText2.classList.toggle('show')
+}
+
+let hideText_btn3 = document.getElementById('hideText_btn3');
+
+let hideText3 = document.getElementById('hideText3')
+
+hideText_btn3.addEventListener('click',toggleText3);
+
+function toggleText3() {
+    hideText3.classList.toggle('show')
+}
+
+let hideText_btn4 = document.getElementById('hideText_btn4');
+
+let hideText4 = document.getElementById('hideText4')
+
+hideText_btn4.addEventListener('click',toggleText4);
+
+function toggleText4() {
+    hideText4.classList.toggle('show')
+}
+
+let hideText_btn5 = document.getElementById('hideText_btn5');
+
+let hideText5 = document.getElementById('hideText5')
+
+hideText_btn5.addEventListener('click',toggleText5);
+
+function toggleText5() {
+    hideText5.classList.toggle('show')
+}
+
+let hideText_btn6 = document.getElementById('hideText_btn6');
+
+let hideText6 = document.getElementById('hideText6')
+
+hideText_btn6.addEventListener('click',toggleText6);
+
+function toggleText6() {
+    hideText6.classList.toggle('show')
+}
+
+let hideText_btn7 = document.getElementById('hideText_btn7');
+
+let hideText7 = document.getElementById('hideText7')
+
+hideText_btn7.addEventListener('click',toggleText7);
+
+function toggleText7() {
+    hideText7.classList.toggle('show')
+}
+
+let hideText_btn8 = document.getElementById('hideText_btn8');
+
+let hideText8 = document.getElementById('hideText8')
+
+hideText_btn8.addEventListener('click',toggleText8);
+
+function toggleText8() {
+    hideText8.classList.toggle('show')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var  fotoMostrada = "add";
+function mifuncion(){
+    var imagen =document.getElementById("hideText_btn")
+    imagen.src="../Iconos/arriba.png"
+    if(fotoMostrada == "add"){
+        imagen.src = "../Iconos/arriba.png";
+        fotoMostrada="arriba"
+    }else{
+        imagen.src = "../Iconos/add.png"
+        fotoMostrada = "add"
+    }
+}
+
+
+var  fotoMostrada2 = "add";
+function mifuncion2(){
+    var imagen2 =document.getElementById("hideText_btn2")
+    imagen2.src="../Iconos/arriba.png"
+    if(fotoMostrada2 == "add"){
+        imagen2.src = "../Iconos/arriba.png";
+        fotoMostrada2="arriba"
+    }else{
+        imagen2.src = "../Iconos/add.png"
+        fotoMostrada2 = "add"
+    }
+}
+
+
+var  fotoMostrada3 = "add";
+function mifuncion3(){
+    var imagen3 =document.getElementById("hideText_btn3")
+    imagen3.src="../Iconos/arriba.png"
+    if(fotoMostrada3 == "add"){
+        imagen3.src = "../Iconos/arriba.png";
+        fotoMostrada3="arriba"
+    }else{
+        imagen3.src = "../Iconos/add.png"
+        fotoMostrada3 = "add"
+    }
+}
+
+var  fotoMostrada4 = "add";
+function mifuncion4(){
+    var imagen4 =document.getElementById("hideText_btn4")
+    imagen4.src="../Iconos/arriba.png"
+    if(fotoMostrada4 == "add"){
+        imagen4.src = "../Iconos/arriba.png";
+        fotoMostrada4="arriba"
+    }else{
+        imagen4.src = "../Iconos/add.png"
+        fotoMostrada4 = "add"
+    }
+}
+
+var  fotoMostrada5 = "add";
+function mifuncion5(){
+    var imagen5 =document.getElementById("hideText_btn5")
+    imagen5.src="../Iconos/arriba.png"
+    if(fotoMostrada5 == "add"){
+        imagen5.src = "../Iconos/arriba.png";
+        fotoMostrada5="arriba"
+    }else{
+        imagen5.src = "../Iconos/add.png"
+        fotoMostrada5 = "add"
+    }
+}
+
+var  fotoMostrada6 = "add";
+function mifuncion6(){
+    var imagen6 =document.getElementById("hideText_btn6")
+    imagen6.src="../Iconos/arriba.png"
+    if(fotoMostrada6 == "add"){
+        imagen6.src = "../Iconos/arriba.png";
+        fotoMostrada6="arriba"
+    }else{
+        imagen6.src = "../Iconos/add.png"
+        fotoMostrada6 = "add"
+    }
+}
+
+var  fotoMostrada7 = "add";
+function mifuncion7(){
+    var imagen7 =document.getElementById("hideText_btn7")
+    imagen7.src="../Iconos/arriba.png"
+    if(fotoMostrada7 == "add"){
+        imagen7.src = "../Iconos/arriba.png";
+        fotoMostrada7="arriba"
+    }else{
+        imagen7.src = "../Iconos/add.png"
+        fotoMostrada7 = "add"
+    }
+}
+
+var  fotoMostrada8 = "add";
+function mifuncion8(){
+    var imagen8 =document.getElementById("hideText_btn8")
+    imagen8.src="../Iconos/arriba.png"
+    if(fotoMostrada8 == "add"){
+        imagen8.src = "../Iconos/arriba.png";
+        fotoMostrada8="arriba"
+    }else{
+        imagen8.src = "../Iconos/add.png"
+        fotoMostrada8 = "add"
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.flexslider').flexslider({
+        pauseOnAction: false,
+        slideshowSpeed: 3500
+    });
+});
+
+
