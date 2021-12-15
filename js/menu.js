fb67e5e6c10bc09607c659ef6170a36abf6c9264
Marco Antonio Martínez Olivares <72892046+Marco2000o@users.noreply.github.com>
2021-12-15 16:38:42 -0600

    Fix-MenuNoScroll

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
 const menu = document.querySelector('.menu');
 const btn = document.querySelector('.menu-btn');
+let numero = 1;
 
     btn.addEventListener('click', () => {
         menu.classList.toggle('active')
+        if(numero == 1){
+            $("body").addClass("no_scroll");
+            numero = 2;
+        }
+        else if(numero==2){
+            $("body").removeClass("no_scroll");
+            numero = 1;
+        }
     })
